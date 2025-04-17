7f8dd14e0acc2406ca26196147ff1322ffb9f19f
suxsen <2296887348@qq.com>
2025-04-17 13:21:36 +0800

    改了话题和速度

--- src/vel_pkg/scripts/vel_node.py
+++ src/vel_pkg/scripts/vel_node.py
@@ -8,9 +8,11 @@ if __name__ == "__main__":
     rospy.init_node("vel_node")
     rospy.logwarn("vel_node is activated")
 
-    vel_pub = rospy.Publisher("cmd/vel", Twist, queue_size=10)
+    # vel_pub = rospy.Publisher("cmd/vel", Twist, queue_size=10) 
+    #一般用这个，公司用的话题不太一样
+    vel_pub = rospy.Publisher("/mobile_base/mobile_base_controller/cmd_vel", Twist, queue_size=10)
     vel_msg = Twist()
-    vel_msg.linear.x = 1
+    vel_msg.linear.x = 0.1
 
     rate = rospy.Rate(30)
 
