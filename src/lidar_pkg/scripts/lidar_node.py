d2a363b7713524efc766881f95ebe4bf9811aefe
hxy <3095895450@qq.com>
2025-04-21 20:42:05 +0800

    复杂转向逻辑

--- src/lidar_pkg/scripts/lidar_node.py
+++ src/lidar_pkg/scripts/lidar_node.py
@@ -7,12 +7,14 @@ from geometry_msgs.msg import Twist
 
 count = 0
 def lidar_callback(msg):
-    global vel_pub #这里全局变量是为啥
-    global count #这里全局变量是为啥
+    global vel_pub ,last_action
+    global count 
     # dist = msg.ranges[180]
     dist_front = msg.ranges[305] #公司的是前180度分成610份，所以正前方应该是305
     dist_left = msg.ranges[609]
+    dist_mid_left = msg.ranges[609]
     dist_right = msg.ranges[0]
+    dist_mid_right = msg.ranges[0]
     vel_cmd = Twist()
     rospy.loginfo("正前方测距数值 = %f 米",dist_front)
     rospy.loginfo("正左方测距数值 = %f 米",dist_left)
@@ -20,10 +22,40 @@ def lidar_callback(msg):
     if count > 0:
         count = count - 1
         rospy.logwarn("持续转向 count = %d",count)
-        vel_cmd.angular.z = 0.4
+        # if 0 < dist_mid_left < 0.3:  # 左前有障碍
+        #         vel_cmd.linear.x = 0.05
+        #         vel_cmd.angular.z = -0.6  # 右转避开
+        #         rospy.logwarn("动态右转避开左侧障碍")
+                
+        # elif 0 < dist_mid_right < 0.3:  # 右前有障碍
+        #     vel_cmd.linear.x = 0.05
+        #     vel_cmd.angular.z = 0.6  # 左转避开
+        #     rospy.logwarn("动态左转避开右侧障碍")
+                
+        else:  # 维持原转向逻辑
+                vel_cmd.angular.z = last_action['angular']  
+                vel_cmd.linear.x = last_action['linear']
+                
+        vel_pub.publish(vel_cmd)
+        return  # 退出，避免进入后续判断
     else:
-        if 0<dist_front < 0.4:
+        if 0<dist_front < 0.4 :
+            if 0<dist_mid_left<0.3 and 0<dist_mid_left<0.3:
+                vel_cmd.linear.x = -0.1
+                vel_cmd.angular.z = 0.4
+                last_action = {'linear': -0.1, 'angular': 0.4}
             vel_cmd.angular.z = 0.4
+            last_action = {'linear': 0, 'angular': 0.4}
+            count = 40
+        elif 0<dist_mid_left<0.5:
+            vel_cmd.linear.x = 0.1
+            vel_cmd.angular.z =-0.4
+            last_action = {'linear': 0.1, 'angular': -0.4}
+            count = 40
+        elif 0<dist_mid_right<0.5:
+            vel_cmd.linear.x = 0.1
+            vel_cmd.angular.z =0.4
+            last_action = {'linear': 0.1, 'angular': 0.4}
             count = 40
         else:
             vel_cmd.linear.x = 0.1
