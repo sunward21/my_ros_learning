74b90027ded82ef077980bd16bc439648ebbfb59
hxy <3095895450@qq.com>
2025-04-28 13:35:56 +0800

    自定义消息包

--- src/atr_pkg/scripts/speed_node.py
+++ src/atr_pkg/scripts/speed_node.py
@@ -3,19 +3,21 @@
 
 import rospy
 from std_msgs.msg import String
-
+from qq_msgs.msg import carry
 def lidar_callback(msg):{
     rospy.logwarn(msg.data)
+
 }
     
-def chao_callback(msg):{
+def chao_callback(msg): 
+    rospy.loginfo(msg.grade)
+    rospy . logwarn(msg.star)
     rospy.loginfo(msg.data)
-}
 
 if __name__ == "__main__":
     rospy.init_node("speed_node")
-    sub = rospy.Subscriber("my_chao_topic", String,  lidar_callback, queue_size= 10 )
-    sub2 = rospy.Subscriber("my_lidar_topic", String,  chao_callback, queue_size= 10 )
+    sub = rospy.Subscriber("my_chao_topic", carry,  chao_callback, queue_size= 10 )
+    sub2 = rospy.Subscriber("my_lidar_topic", carry,  lidar_callback, queue_size= 10 )
     while not rospy.is_shutdown():{
         rospy.spin()
     }
--- src/ssr_pkg/scripts/chao_node.py
+++ src/ssr_pkg/scripts/chao_node.py
@@ -3,17 +3,19 @@
 
 import rospy
 from std_msgs.msg import String
-
+from qq_msgs.msg import carry
 if __name__ == "__main__":
     rospy.init_node("chao_node")
     rospy.logwarn("chao!py!")
 
-    pub = rospy.Publisher("my_chao_topic", String, queue_size=10)
+    pub = rospy.Publisher("my_chao_topic", carry, queue_size=10)
     rate = rospy.Rate(10)
 
     while not rospy.is_shutdown():
         rospy.loginfo("chao!!!py")
-        msg = String()
+        msg = carry()
         msg.data = "msg_of_chao_py"
+        msg.star = 10
+        msg.grade = "wangzhe"
         pub.publish(msg)
         rate.sleep()
