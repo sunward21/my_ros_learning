c35830f5e2efe860c72f68099aa276f2a6d0ad0b
hxy <3095895450@qq.com>
2025-04-28 13:34:01 +0800

    移除编译目录的Git跟踪

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/handsfree/catkin_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/handsfree/catkin_ws/devel;/home/handsfree/handsfree/handsfree_ros_ws/devel;/opt/ros/melodic'.split(';') if '/home/handsfree/catkin_ws/devel;/home/handsfree/handsfree/handsfree_ros_ws/devel;/opt/ros/melodic' != '' else []
--- build/vel_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/handsfree/catkin_ws/src/vel_pkg
-# Build directory: /home/handsfree/catkin_ws/build/vel_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
